de33c79fcaef85b3f0393d29d14d94de0f88e62a
Ben Ellis <benx45h@gmail.com>
2020-12-10 02:19:47 -0500

    Day 10

---
+++ 10/script.py
@@ -0,0 +1,19 @@
+ifile = open('input.txt', 'r')
+lines = file.readlines()
+jlist = [int(i) for i in lines]
+
+jlist.sort()
+
+for i in range(0, len(jlist)):
+    if(i == 0):
+        diff = 1
+    if(i > 0):
+        diff = jlist[i] - jlist[i-1]
+    if(diff == 1):
+        d1 = d1 + 1
+    if(diff == 3):
+        d3 = d3 + 1
+
+d3 = d3 + 1
+
+print(d3*d1)
---
+++ 10/script2.py
@@ -0,0 +1,35 @@
+ifile = open('input.txt', 'r')
+lines = ifile.readlines()
+jlist = [int(i) for i in lines]
+jlist.append(0)
+jlist.sort()
+
+
+options = []
+options.append(1)
+for i in jlist:
+    a1 = ((i+1) in jlist)
+    a2 = ((i+2) in jlist)
+    a3 = ((i+3) in jlist)
+    fuck = a1 + a2 + a3
+    if(i == max(jlist)):
+        fuck = 1
+    options.append(fuck)
+
+def sum_branch(options, idx):
+    s = 0
+    pos = options[idx]
+    if(pos == 1):
+        return 1
+    for i in range(1, pos+1):
+        count = sum_branch(options, (idx + i))
+        s = s + count
+    return s
+
+yeet = 1
+for i in range(1,len(options)):
+    if(options[i] > 1) and options[i-1] <= 1:
+       count = sum_branch(options, i)
+       yeet = yeet * count
+
+print(yeet)
